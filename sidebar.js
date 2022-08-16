b9b7bce48e1c293ef45705d7d80ef91b1084d573
Leila Pan <leila.pan@stonybrook.edu>
2022-08-15 20:20:32 -0400

    Added sidebar

---
+++ sidebar.js
@@ -0,0 +1,8 @@
+function sidebar_open(){
+    document.getElementById("sidebaropen").style.width="100%";
+    document.getElementById("sidebaropen").style.display="block";
+}
+
+function sidebar_close() {
+    document.getElementById("sidebaropen").style.display = "none";
+  }
